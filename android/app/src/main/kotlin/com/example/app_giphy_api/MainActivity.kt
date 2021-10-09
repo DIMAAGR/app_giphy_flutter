b0c812f75c962e210e0f47faee98b086623305cf
Italo Matos <attackdiamond@gmail.com>
2021-10-09 00:00:34 -0300

    Projeto Iniciado

---
+++ android/app/src/main/kotlin/com/example/app_giphy_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_giphy_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_giphy_api/views/main/main_screen.designer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
