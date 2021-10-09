eec9ebcf4fa6075747f6e3eac428d63f4f0c127c
Italo Matos <attackdiamond@gmail.com>
2021-10-09 12:32:24 -0300

    codigo refatorado

--- lib/views/main/main_screen.designer.dart
+++ lib/views/main/main_screen.designer.dart
@@ -1,12 +1,31 @@
 import 'package:app_giphy_api/components/circular_button.dart';
+import 'package:app_giphy_api/components/gif_topic_button.dart';
+import 'package:app_giphy_api/data/api_request.dart';
+import 'package:app_giphy_api/data/data_requests.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  DataRequest _dataRequest = DataRequest();
+  @override
+  void initState() {
+    super.initState();
+    print("SARTED!");
+    // _dataRequest.requestData(null, "0", "1").then((map) {
+    //   print(map);
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    ApiRequest _apiRequest = ApiRequest(context: context);
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 245, 245, 245),
       appBar: AppBar(
@@ -38,9 +57,57 @@ class MainScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            // É NECESSÁRIO UM FUTURE BUILDER PARA CARREGAR AS INFORMAÇÕES
+            // DA API DENTRO DA INTERFACE
+            _topicButton(context, _apiRequest, null, "Trending"),
+            _topicButton(context, _apiRequest, "reactions", "Reactions"),
+            _topicButton(
+                context, _apiRequest, "entertainment", "Entertainment"),
+            _topicButton(context, _apiRequest, "sports", "Sports"),
+            _topicButton(context, _apiRequest, "Stickers", "Stickers"),
+            _topicButton(context, _apiRequest, "Artists", "Artists"),
+          ],
         ),
       ),
     );
+  }
+
+  _topicButton(BuildContext context, ApiRequest apiRequest, String? search,
+      String topicName) {
+    return FutureBuilder(
+      future: _dataRequest.requestData(search, "0", "1"),
+      builder: (context, snapshot) => _topics(
+          context: context,
+          snapshot: snapshot,
+          apiRequest: apiRequest,
+          onTap: () {},
+          topicName: topicName),
+    );
+  }
+
+  _topics({
+    required BuildContext context,
+    required AsyncSnapshot snapshot,
+    required ApiRequest apiRequest,
+    required String topicName,
+    required void Function() onTap,
+  }) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.active:
+      case ConnectionState.done:
+        return TopicsGifButton(
+            context: context,
+            snapshot: snapshot,
+            topicName: topicName,
+            onTap: onTap);
+      default:
+        return apiRequest.sendWaitOrErrorMessage(
+          snapshot: snapshot,
+        );
+    }
   }
 }
