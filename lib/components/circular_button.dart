412e91c1815dde47340f96b288a8465f5bd41045
Italo Matos <attackdiamond@gmail.com>
2021-10-11 13:27:42 -0300

    Atualizado Button por causa da atualização do Flutter para 2.5.2

--- lib/components/circular_button.dart
+++ lib/components/circular_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CircularButton extends StatelessWidget {
   final double padding;
-  final Icon icon;
+  final Widget icon;
   final void Function() onPressed;
   const CircularButton({
     Key? key,
@@ -10,6 +10,7 @@ class CircularButton extends StatelessWidget {
     required this.icon,
     required this.onPressed,
   }) : super(key: key);
+
   // Outra forma de se fazer um botão circular usando o
   // Elevated Button
   @override
@@ -19,18 +20,32 @@ class CircularButton extends StatelessWidget {
       child: Container(
         height: 40,
         width: 40,
-        child: ElevatedButton(
-          onPressed: onPressed,
-          child: icon,
-          style: ElevatedButton.styleFrom(
-            shape: CircleBorder(),
-            elevation: 3,
-            padding: EdgeInsets.all(16),
-            shadowColor: Color.fromARGB(255, 60, 60, 60),
-            primary: Color.fromARGB(255, 245, 245, 245),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(40),
+          onTap: onPressed,
+          child: Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 240, 240, 240),
+                borderRadius: BorderRadius.circular(40)),
+            child: icon,
           ),
         ),
       ),
     );
   }
 }
+
+// ElevatedButton(
+//           onPressed: onPressed,
+//           child: icon,
+//           style: ElevatedButton.styleFrom(
+//             alignment: Alignment.center,
+//             shape: CircleBorder(),
+//             elevation: 3,
+//             padding: EdgeInsets.all(16),
+//             shadowColor: Color.fromARGB(255, 60, 60, 60),
+//             primary: Color.fromARGB(255, 245, 245, 245),
+//           ),
+//         ),
