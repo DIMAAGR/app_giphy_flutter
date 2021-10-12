f786a251a42944fcfc4193f5ec1706a7c25c953a
Italo Matos <attackdiamond@gmail.com>
2021-10-12 13:33:08 -0300

    Adicionado A rota settings

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:app_giphy_api/views/gif_view/gif_view.designer.dart';
 import 'package:app_giphy_api/views/image_list/image_list.designer.dart';
 import 'package:app_giphy_api/views/main/main_screen.designer.dart';
 import 'package:app_giphy_api/views/search_view/search_view.desiger.dart';
+import 'package:app_giphy_api/views/settings_view/settings_view.designer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.GIF_LIST: (ctx) => GifList(),
           AppRoutes.GIF_VIEW: (ctx) => GifView(),
           AppRoutes.SEARCH_VIEW: (ctx) => SearchView(),
+          AppRoutes.SETTINGS_VIEW: (ctx) => SettingsView(),
         },
       ),
     );
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -3,4 +3,5 @@ class AppRoutes {
   static const GIF_LIST = '/gif-list';
   static const GIF_VIEW = '/gif-view';
   static const SEARCH_VIEW = '/search-view';
+  static const SETTINGS_VIEW = '/settings-view';
 }
