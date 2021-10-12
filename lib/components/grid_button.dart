bccf93badc5e1697bb18ff2f9037ae08bf5ddc16
Italo Matos <attackdiamond@gmail.com>
2021-10-12 11:59:22 -0300

    Agora o GridButon transfere as informações do gif para a variavel GifProperty

--- lib/components/grid_button.dart
+++ lib/components/grid_button.dart
@@ -1,4 +1,8 @@
+import 'package:app_giphy_api/data/gif_property.dart';
+import 'package:app_giphy_api/store/app_giphy_api.store.dart';
+import 'package:app_giphy_api/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GridButton extends StatelessWidget {
   const GridButton({
@@ -13,9 +17,18 @@ class GridButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppGiphyApi _provider = Provider.of<AppGiphyApi>(context);
     return InkWell(
       onTap: () {
-        print(snapshot.data["data"][index]["images"]["original"]["url"]);
+        // ACTIONS
+        _provider.changeGif(GifProperty(
+            gifUrl: snapshot.data["data"][index]["images"]["original"]["url"],
+            hdMp4Url: snapshot.data["data"][index]["images"]["original"]["mp4"],
+            p480Mp4Url: snapshot.data["data"][index]["images"]["480w_still"]
+                ["url"],
+            username: snapshot.data["data"][index]["username"],
+            title: snapshot.data["data"][index]["title"]));
+        Navigator.of(context).pushNamed(AppRoutes.GIF_VIEW);
       },
       child: Container(
         decoration: BoxDecoration(
