feaa2e9fa13dc6b445331bdbd9c37b72abe254c7
Italo Matos <attackdiamond@gmail.com>
2021-10-13 08:55:49 -0300

    agora é possível compartilhar o gif

--- lib/views/gif_view/gif_view.designer.dart
+++ lib/views/gif_view/gif_view.designer.dart
@@ -6,6 +6,7 @@ import 'package:app_giphy_api/store/app_giphy_api.store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:share/share.dart';
 
 class GifView extends StatelessWidget {
   const GifView({Key? key}) : super(key: key);
@@ -42,87 +43,71 @@ class GifView extends StatelessWidget {
     required ApiRequest api,
     required BuildContext context,
   }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            provider.gifProperty.title,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color.fromARGB(255, 60, 60, 60),
-              fontFamily: "Montserrat",
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              height: 400,
-              width: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Color.fromARGB(255, 230, 230, 230),
-                image: DecorationImage(
-                    image: NetworkImage(provider.gifProperty.gifUrl),
-                    fit: BoxFit.cover),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            provider.gifProperty.username != ""
-                ? "By: " + provider.gifProperty.username
-                : "",
-            style: TextStyle(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              provider.gifProperty.title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
                 color: Color.fromARGB(255, 60, 60, 60),
                 fontFamily: "Montserrat",
-                fontSize: 16,
-                fontWeight: FontWeight.w300,
-                fontStyle: FontStyle.italic),
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularButton(
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: 400,
+                width: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Color.fromARGB(255, 230, 230, 230),
+                  image: DecorationImage(
+                      image: NetworkImage(provider.gifProperty.gifUrl),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              provider.gifProperty.username != ""
+                  ? "By: " + provider.gifProperty.username
+                  : "",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 60, 60, 60),
+                  fontFamily: "Montserrat",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularButton(
                 padding: 8,
-                width: 87 + 16,
-                border: BorderRadius.circular(16),
                 color: Color.fromARGB(255, 240, 240, 240),
-                icon: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.download,
-                      size: 24,
-                    ),
-                    Text("Download"),
-                  ],
+                icon: Icon(
+                  Icons.share,
+                  size: 24,
                 ),
                 onPressed: () {
-                  download(provider.gifProperty.hdMp4Url,
-                      provider.gifProperty.title);
-                  print("downloaded!");
-                }),
-            CircularButton(
-              padding: 8,
-              color: Color.fromARGB(255, 240, 240, 240),
-              icon: Icon(
-                Icons.share,
-                size: 24,
-              ),
-              onPressed: () {},
-            )
-          ],
-        ),
-      ],
+                  Share.share(provider.gifProperty.gifUrl);
+                },
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
